07ffc74986780cf4e61f1a6bdd94ded9860f9b47
Entroperiance.J <bmsac0001@gmail.com>
2023-12-14 08:04:13 +0800

    [entropy-make-core.sh] use debian as base image

--- entropy-make-via-docker.sh
+++ entropy-make-via-docker.sh
@@ -18,6 +18,73 @@ MK_UGROUP_ID="$(id -g)"
 
 MK_MOUTPONIT="/home/build/entropy-floorp-build/"
 
+
+MK_DEBIAN_SROUCELIST='
+deb     https://mirrors.ustc.edu.cn/debian/         bookworm main contrib non-free non-free-firmware
+deb-src https://mirrors.ustc.edu.cn/debian/         bookworm main contrib non-free non-free-firmware
+
+deb     https://mirrors.ustc.edu.cn/debian-security bookworm-security main contrib non-free non-free-firmware
+deb-src https://mirrors.ustc.edu.cn/debian-security bookworm-security main contrib non-free non-free-firmware
+
+deb     https://mirrors.ustc.edu.cn/debian/         bookworm-updates main contrib non-free non-free-firmware
+deb-src https://mirrors.ustc.edu.cn/debian/         bookworm-updates main contrib non-free non-free-firmware
+
+deb     https://mirrors.ustc.edu.cn/debian/         bookworm-backports main contrib non-free non-free-firmware
+deb-src https://mirrors.ustc.edu.cn/debian/         bookworm-backports main contrib non-free non-free-firmware
+'
+
+MK_DEBIAN_DEPS=(
+    # deps from moz boostrap
+    bash findutils gzip libxml2 m4 make perl tar unzip git
+    # deps from debian firefox build deps
+    autotools-dev
+    debhelper-compat
+    libx11-dev
+    libx11-xcb-dev
+    libxt-dev
+    libgtk-3-dev
+    libglib2.0-dev
+    libdrm-dev
+    libstartup-notification0-dev
+    libjpeg-dev
+    zlib1g-dev
+    libreadline-dev
+    python3
+    python3-pip
+    python-is-python3
+    dpkg-dev
+    libnspr4-dev
+    libnss3-dev
+    libvpx-dev
+    libdbus-glib-1-dev
+    libffi-dev
+    libevent-dev
+    libpulse-dev
+    libasound2-dev
+    yasm
+    nasm
+    llvm-14-dev
+    libclang-14-dev
+    clang-14
+    libc++-14-dev-wasm32
+    libclang-rt-14-dev-wasm32
+    lld-14
+    cbindgen
+    nodejs
+    zip
+    unzip
+    locales
+    xvfb
+    xfonts-base
+    xauth
+    ttf-bitstream-vera
+    fonts-freefont-ttf
+    fonts-dejima-mincho
+    iso-codes
+    # entropy spec deps
+    util-linux coreutils passwd curl
+)
+
 # we should add a fake host entry to prevent pacman replace
 # hosts.pacnew to dedicated /etc/hosts (readonly with post error) file
 # since we use host machine's network stack.
@@ -26,26 +93,38 @@ docker run \
        --network=host \
        --add-host='localhost:127.0.0.1' \
        -v "${MK_BASHSRCDIR}:${MK_MOUTPONIT}" \
-       archlinux:base-devel \
+       debian:bookworm \
        bash -c \
        "set -e; \
-echo 'Server = https://mirrors.ustc.edu.cn/archlinux/\$repo/os/\$arch' > /etc/pacman.d/mirrorlist
-pacman-key --init ; pacman -Syy --noconfirm ; pacman -S --noconfirm archlinux-keyring ; \
-pacman -Syu --noconfirm ; \
-pacman -S --noconfirm --needed bash findutils gzip \
-libxml2 m4 make perl tar unzip git xorg-server-xvfb \
-python python-pip; \
+if [ -d /usr/lib/apt/methods ] &&                                                            \
+       [ ! -e /usr/lib/apt/methods/https ] ; then                                            \
+    cd /usr/lib/apt/methods ;                                                                \
+    ln -s http https ;                                                                       \
+fi ;                                                                                         \
+if [ -f /etc/apt/sources.list ]; then                                                        \
+    sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list ;                  \
+elif [ -f /etc/apt/sources.list.d/debian.sources ] ; then                                    \
+    sed -i 's/deb.debian.org/mirrors.ustc.edu.cn/g' /etc/apt/sources.list.d/debian.sources ; \
+else                                                                                         \
+    printf 'No apt source list or referred file found' ;                                     \
+    exit 1 ;                                                                                 \
+fi ;                                                                                         \
+apt-get -y update ; \
+apt install -y ca-certificates apt-transport-https; \
+echo '${MK_DEBIAN_SROUCELIST}' > /etc/apt/sources.list ; \
+apt-get -y update ; apt-get -y upgrade ; \
+apt install -y ${MK_DEBIAN_DEPS[*]} ; \
 groupadd -g '$MK_UGROUP_ID' '$MK_UGROUP_NAME'; \
-useradd  -m -G '$MK_UGROUP_NAME' -g '$MK_UGROUP_ID' -u '$MK_USER_ID' '$MK_USER_NAME';
-su '$MK_USER_NAME' -c 'cd '${MK_MOUTPONIT}';
-export HTTP_PROXY=\"${HTTP_PROXY}\";
-export http_proxy=\"${http_proxy}\";
-export HTTPS_PROXY=\"${HTTPS_PROXY}\";
-export https_proxy=\"${https_proxy}\";
-export NO_PROXY=\"${NO_PROXY}\";
-export no_proxy=\"${no_proxy}\";
-export -p ;
-echo \"Test internet capable ...\" ;
-curl -I www.google.com ;
-echo \"with user \$(id -a) ...\" ; sleep 10 ;
-bash entropy-make-core.sh ;'"
+useradd  -m -G '$MK_UGROUP_NAME' -g '$MK_UGROUP_ID' -u '$MK_USER_ID' '$MK_USER_NAME'; \
+su '$MK_USER_NAME' -c \"cd '${MK_MOUTPONIT}'; \
+export HTTP_PROXY='${HTTP_PROXY}';            \
+export http_proxy='${http_proxy}';            \
+export HTTPS_PROXY='${HTTPS_PROXY}';          \
+export https_proxy='${https_proxy}';          \
+export NO_PROXY='${NO_PROXY}';                \
+export no_proxy='${no_proxy}';                \
+export -p ;                                   \
+echo 'Test internet capable ...' ;            \
+curl -I www.google.com ;                      \
+echo 'with user \$(id -a) ...' ; sleep 10 ;   \
+bash entropy-make-core.sh ;\""
